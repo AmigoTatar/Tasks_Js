bc9164aa0fc43124507d343b5dcdaa0620bc00e7
AmigoTatar <124tbtm@gmail.com>
2022-10-21 12:46:54 +0500

    string_in_number

---
+++ string_in_number/string_in_number/js/main.js
@@ -0,0 +1,11 @@
+
+// преобразование массива в строк в число
+
+
+const stringToNumber = function(str){
+    str = ['123445465','55'];
+     nas= str;
+   return +nas.join('');
+}
+console.log(stringToNumber())
+
